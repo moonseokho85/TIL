418198dca96de11f201352573e88fd7753154c0f
moonseokho85 <mseokho7733@gmail.com>
2020-01-30 14:25:55 +0900

    add LSTM1.py and keras17_scalar1.py

--- A.I/10_Keras/keras16_LSTM1.py
+++ A.I/10_Keras/keras16_LSTM1.py
@@ -1,6 +1,6 @@
 from numpy import array
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, LSTM, Reshape
 
 # 데이터 준비
 x = array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8], [7, 8, 9], [8, 9, 10], [9, 10, 11], [10, 11, 12], [20, 30, 40], [30, 40, 50], [40, 50, 60]])
@@ -14,15 +14,29 @@ x = x.reshape(x.shape[0], x.shape[1], 1)
 
 # 모델 구성
 model = Sequential()
-model.add(LSTM(64, activation='relu', input_shape=(3, 1))) # 3: column number & 1: 몇개씩 자르는지
-model.add(Dense(128))
-model.add(Dense(256))
-model.add(Dense(512))
-model.add(Dense(256))
-model.add(Dense(128))
-model.add(Dense(64))
+model.add(LSTM(10, activation='relu', input_shape=(3, 1), return_sequences=True)) # 3: column number & 1: 몇개씩 자르는지
+model.add(LSTM(2, activation='relu', return_sequences=True))
+model.add(LSTM(3, activation='relu', return_sequences=True))
+model.add(LSTM(4, activation='relu', return_sequences=True))
+model.add(LSTM(5, activation='relu', return_sequences=True))
+model.add(LSTM(6, activation='relu', return_sequences=True))
+model.add(LSTM(7, activation='relu', return_sequences=True))
+model.add(LSTM(8, activation='relu', return_sequences=True))
+model.add(LSTM(9, activation='relu', return_sequences=True))
+model.add(LSTM(10, activation='relu', return_sequences=True)) # LSTM을 많이 구성하면 시계열 데이터의 특성을 잃을 수 있습니다.
+model.add(LSTM(5, activation='relu'))
+model.add(Dense(32, activation='linear'))
+model.add(Dense(1))
+
+'''
+# Reshape 모델 사용
+model = Sequential()
+model.add(LSTM(10, activation='relu', input_shape=(3, 1)))
+model.add(Reshape((3, 1), input_shape=(64,)))
+model.add(LSTM(10, activation='relu'))
 model.add(Dense(32))
 model.add(Dense(1))
+'''
 
 model.summary()
 

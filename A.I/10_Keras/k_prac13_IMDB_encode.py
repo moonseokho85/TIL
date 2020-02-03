abb5875b847c51798ed703c5c29001f01367129a
seokhomoon <mseokho7733@gmail.com>
2020-02-03 20:58:09 +0900

    modify predious files and new file

--- A.I/10_Keras/k_prac13_IMDB_encode.py
+++ A.I/10_Keras/k_prac13_IMDB_encode.py
@@ -6,7 +6,7 @@ print(train_data[0])
 print(train_labels[0])
 
 print(max([max(sequence) for sequence in train_data]))
-
+'''
 word_index = imdb.get_word_index # word_index는 단어와 정수 인덱스를 매핑한 딕셔너리입니다.
 # print(word_index)
 
@@ -15,4 +15,26 @@ reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]]) # 리뷰를 디코딩합니다. 0, 1, 2는 '패딩', '문서 시작', '사전에 없음'을 위한 인덱스이므로 3을 뺍니다.
 # print(decoded_review)
+'''
+
+import numpy as np
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1. # result[i]에서 특정 인덱스의 위치를 1로 만듭니다.
+    return results
+
+x_train = vectorize_sequences(train_data)
+
+x_test = vectorize_sequences(test_data)
+
+print(x_train[0]) # (10000, )
+
+# train_labes 타입 확인
+print(train_labels.dtype)
+
+y_train = np.asarray(train_labels).astype('float32')
+
+y_test = np.asarray(test_labels).astype('float32')
 

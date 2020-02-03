2d80891bea7eb724e04e2ea145cb141a0cd6c481
seokhomoon <mseokho7733@gmail.com>
2020-02-03 23:57:45 +0900

    modify a previous file.py

--- A.I/10_Keras/k_prac16_IMDB_remodel.py
+++ A.I/10_Keras/k_prac16_IMDB_remodel.py
@@ -46,7 +46,15 @@ model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+model.summary()
+
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 
 model.fit(x_train, y_train, epochs=4, batch_size=512)
-results = model.predict(x_test, y_test)
+print('Training finish')
+
+results = model.evaluate(x_test, y_test)
+print(results)
+
+y_pred = model.predict(x_test)
+print(y_pred)

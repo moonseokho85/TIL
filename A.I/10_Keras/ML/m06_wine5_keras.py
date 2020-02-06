ed75c0a947e3395f273dfe0383c590bcdba0c53f
moonseokho85 <mseokho7733@gmail.com>
2020-02-06 14:33:32 +0900

    modify m06_wine4.py and m06_wine5_keras.py

--- A.I/10_Keras/ML/m06_wine5_keras.py
+++ A.I/10_Keras/ML/m06_wine5_keras.py
@@ -19,11 +19,42 @@ for v in list(y):
     else:
         newlist += [2]
 y = newlist
- 
+
 # Split Data to Train and Test
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
+# One-Hot Encoding
+from keras.utils import np_utils
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+print(y_train)
+print(y_test)
+print(y_train.shape)
+print(y_test.shape)
+
+# Model
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(256, input_shape=(11, ), activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+
+# Model Compiling
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Model Fitting
+model.fit(x_train, y_train, batch_size=5, epochs=200, validation_split=0.2)
+
+# Print Result
+print("\n Accuracy: %.4f" % (model.evaluate(x_test, y_test)[1]))
+
+'''
 # Making model
 from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier()
@@ -41,3 +72,4 @@ print("정답률: ", accuracy_score(y_test, y_pred))
 
 from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
+'''

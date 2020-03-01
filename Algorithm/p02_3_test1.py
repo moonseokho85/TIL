f91ba79e793154710bf1680569d9399acbcf56ce
seokhomoon <mseokho7733@gmail.com>
2020-03-01 10:48:08 +0900

    add files about find max value index and min value

---
+++ Algorithm/p02_3_test1.py
@@ -0,0 +1,12 @@
+# 최솟값을 구하기
+
+def find_min(a):
+    n = len(a)
+    min_value = a[0]
+    for i in range(1, n):
+       if a[i] < min_value:
+           min_value = a[i]
+    return min_value
+
+v = [17, 92, 18, 33, 58, 7, 33, 42]
+print(find_min(v))

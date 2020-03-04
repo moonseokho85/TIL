7efb147b06a1266baf46036ffb280131cddf6f7e
seokhomoon <mseokho7733@gmail.com>
2020-03-04 11:09:48 +0900

    modify p04_3_test2.py

--- Algorithm/p04_3_test2.py
+++ Algorithm/p04_3_test2.py
@@ -1,6 +1,6 @@
 # 숫자 n개 중에서 최댓값 찾기를 재귀 호출로 만들기
 
-def find_max(a, n):
+def find_max(a, n): # 리스트 a의 앞부분 n개 중 최댓값을 구하는 재귀 함수
     if n == 1:
         return a[0]
     max_n_1 = find_max(a, n - 1) # n - 1개 중 최댓값을 구함

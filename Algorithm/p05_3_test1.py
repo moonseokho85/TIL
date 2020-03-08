ad99a01edf671ac8adabc5d2d5c1e6272664b28b
seokhomoon <mseokho7733@gmail.com>
2020-03-08 22:03:20 +0900

    add new files about reccurent syntax example

--- Algorithm/p05_3_test1.py
+++ Algorithm/p05_3_test1.py
@@ -1,12 +1,24 @@
-# n번째 피보나치 수 구하기
+# n번째 피보나치 수열 구하기
 # 입력: n
 # n번째 피보나치 수
 
-def find_number(x):
+def find_number(n):
     a = [0, 1]
     for i in range(100):
         value = a[i] + a[i + 1]
         a.append(value)
-    return print(a[x])
+    return print(a[n])
         
 find_number(5)
+
+# n번째 피보나치 수열 찾기 (재귀 용법)
+# 입력: n 값(0부터 시작)
+# 출력: n번째 피보나치 수열 값
+
+def fibo(n):
+    if n <= 1:
+        return n # n = 0 -> 0 | n = 1 -> 1
+    return fibo(n - 2) + fibo(n - 1)
+
+print(fibo(7))
+print(fibo(10))

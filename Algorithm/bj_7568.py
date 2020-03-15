2b18973ddb46ab378c5d2cda1359eb9b46e42040
seokhomoon <mseokho7733@gmail.com>
2020-03-15 16:33:53 +0900

    add new files about baekjun algorithms

---
+++ Algorithm/bj_7568.py
@@ -0,0 +1,52 @@
+# 백준 7568번
+# 각 덩치 등수 구하기
+# 입력: 5
+#       55 185
+#       58 183
+#       88 186
+#       60 175
+#       46 155
+# 출력: 2 2 1 2 5
+'''
+def rank():
+    
+    # input
+    n = int(input())
+    wah = []
+    for i in range(0, n):
+        wah.append(input().split(' '))
+        # convert string to int
+        for j in wah:
+            print(j)
+    print("wah: ", wah)
+    
+rank()
+'''
+
+N = input()     # 입력값 변수
+Answer = []     # 정답에 대한 리스트 변수
+Data = []       # 입력을 받은 데이터를 저장하는 리스트 변수
+
+# 데이터를 리스트에 저장하기
+for i in range(int(N)):
+    list = input().split(" ");
+    Data.append(list);    
+
+# 문제를 풀기 위한 알고리즘 ( 2중 반복문 )
+for i in range(int(N)):
+    count =0;               # 덩치 클 경우 카운트 증가하는 변수
+    for j in range(int(N)):
+        # 똑같은 데이터는 비교하지 않은다.
+        if i == j :
+            continue;
+        else:       # 데이터가 다른 경우
+            if Data[i][0] < Data[j][0] and Data[i][1] < Data[j][1]:
+                # 키와 몸무게가 상대방이 둘다 큰 경우 
+                count = count + 1;
+    Answer.append(count)    # 카운트를 저장
+    
+string = ""     # 결과 출력 변수
+# 결과 포맷에 맞게 출력
+for i in range(int(N)):
+    string = string + str(Answer[i]+1) + " "
+print(string)    
--- Algorithm/p14_2_test1.py
+++ Algorithm/p14_2_test1.py
@@ -1,3 +1,4 @@
+'''
 def find_name(x):
     name_dict = {
         39: "Justin",
@@ -13,3 +14,25 @@ def find_name(x):
 
 print(find_name(39))
 print(find_name(1))
+'''
+
+# 학생 번호에 해당하는 학생 이름 찾기(dict 이용)
+# 입력: 학생 명부 딕셔너리 s_info, 찾는 학생 번호 find_no
+# 출력: 해당하는 학생 이름, 해당하는 학생 번호가 없으면 물음표 "?"
+
+def get_name(s_info, find_no):
+
+    if find_no in s_info:
+        return s_info[find_no]
+    else:
+        return "?" # 해당하는 번호가 없으면 물음표
+
+sample_info = {
+    39: "Justin",
+    14: "John",
+    67: "Mike",
+    105: "Summer"
+}
+
+print(get_name(sample_info, 105))
+print(get_name(sample_info, 777))

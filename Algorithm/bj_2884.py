37a1522d38179c63745324e531e7f007d134325c
seokhomoon <mseokho7733@gmail.com>
2020-03-16 14:34:07 +0900

    modify previous file about baekjoon Algorithm No.2884

--- Algorithm/bj_2884.py
+++ Algorithm/bj_2884.py
@@ -2,6 +2,7 @@
 # Input: Wake up time
 # Output: alarm time before 45 minutes
 
+'''
 # input
 n = input()
 
@@ -13,22 +14,41 @@ converted_splited_n = [int(i) for i in splited_n]
 
 # if phrase
 if (converted_splited_n[0] - 1) > 0 and (converted_splited_n[1] - 45) < 0:
-    remained_minute = converted_splited_n[1] - 45
-    minute = 60 + remained_minute
+    left_minute = converted_splited_n[1] - 45
+    minute = 60 + left_minute
     converted_splited_n[1] = minute
     converted_splited_n[0] = converted_splited_n[0] - 1
     print(str(converted_splited_n[0]) + ' ' + str(converted_splited_n[1]))
     
-elif (converted_splited_n[0] - 1) < 0 and (converted_splited_n[1] - 45) < 0:
+elif (converted_splited_n[0] - 1) <= 0 and (converted_splited_n[1] - 45) < 0:
     remained_hour = converted_splited_n[0] - 1
     hour = 24 + remained_hour
     converted_splited_n[0] = hour
-    remained_minute = converted_splited_n[1] - 45
-    minute = 60 + remained_minute
+    left_minute = converted_splited_n[1] - 45
+    minute = 60 + left_minute
     converted_splited_n[1] = minute
     print(str(converted_splited_n[0]) + ' ' + str(converted_splited_n[1]))
     
 else:
     converted_splited_n[1] = converted_splited_n[1] - 45
     print(str(converted_splited_n[0]) + ' ' + str(converted_splited_n[1]))
-    
+'''
+
+N = input();
+
+splited_n = N.split(" ");
+converted_splited_n = [int(i) for i in splited_n];
+
+totalMinute = converted_splited_n[0] * 60 + converted_splited_n[1];
+# print(totalMinute)
+
+AlarmMinute = totalMinute - 45;
+# print(AlarmMinute)
+
+if AlarmMinute < 0:
+    AlarmMinute = AlarmMinute + 24 * 60;
+
+Hour = int(AlarmMinute / 60);
+Minute = int(AlarmMinute % 60);
+
+print( str(Hour) + " " + str(Minute))

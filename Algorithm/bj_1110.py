c67d8265a7084196c6361671ef528b117792ac1d
seokhomoon <mseokho7733@gmail.com>
2020-03-22 21:10:43 +0900

    modify file about baekjoon No.1110

--- Algorithm/bj_1110.py
+++ Algorithm/bj_1110.py
@@ -4,10 +4,13 @@ N = sys.stdin.readline()
 
 n = int(N)
 
+answer = n
+
+count = 0
+
 while True:
 
     if n < 10: # if n = 9
-        print("n < 10")
         n = str(0) + str(n) # 09
         converted_n = list(n) # ['0', '9']
         sum = int(converted_n[0]) + int(converted_n[1]) # 9
@@ -15,13 +18,14 @@ while True:
         converted_sum = list(sum) # ['9']
         next_n = converted_n[-1] + converted_sum[-1]
         next_n = int(next_n)
-        if next_n == n:
+        n = int(n)
+        count += 1
+        if next_n == answer:
             break
         else:
-            next_n = n
+            n = next_n
     
     else:
-        print("n > 10")
         n = str(n)
         converted_n = list(n) # ['9', '9']
         sum = int(converted_n[0]) + int(converted_n[1]) # 9
@@ -29,7 +33,11 @@ while True:
         converted_sum = list(sum) # ['9']
         next_n = converted_n[-1] + converted_sum[-1]
         next_n = int(next_n)
-        if next_n == n:
+        n = int(n)
+        count += 1
+        if next_n == answer:
             break
         else:
-            next_n = n
+            n = next_n
+            
+print(count)

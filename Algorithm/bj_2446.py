c827327bc1b9874cbeab34bffc3154a533b4170e
seokhomoon <mseokho7733@gmail.com>
2020-03-23 17:01:00 +0900

    add new files about python algorithms

---
+++ Algorithm/bj_2446.py
@@ -0,0 +1,28 @@
+# Baekjun Algorithm No.2523
+# Print star
+# Input: N
+# Output: '*'
+
+import sys
+
+# 입력
+N = sys.stdin.readline()
+
+# int형으로 변환
+n = int(N)
+
+# 2n - 1까지 출력
+converted_n = 2 * n - 1
+
+star = ''
+
+for i in range(1, converted_n + 1):
+    # for문의 i가 n보다 작거나 같으면
+    if i <= n:
+        # 별을 더해준다
+        star += '*'
+    # 아니면
+    else:
+        # 별을 빼준다
+        star = star[:-1]
+    print(star)
--- Algorithm/p15_3_test1.py
+++ Algorithm/p15_3_test1.py
@@ -1,19 +1,19 @@
 
 def print_all_number1(g, start):
     
-    qu = []
-    done = set()
+    qu = []         # 저장 장소 1: 앞으로 처리해야 할 숫자를 큐에 저장
+    done = set()    # 저장 장소 2: 이미 큐에 추가된 숫자를 집합에 저장
     
-    qu.append(start)
-    done.add(start)
+    qu.append(start)    # 자신을 큐에 넣고 시작
+    done.add(start)     # 집합에도 추가
     
-    while qu:
-        p = qu.pop(0)
-        print(p)
-        for x in g[p]:
-            if x not in done:
-                qu.append(x)
-                done.add(x)
+    while qu:                # 큐에 처리해야 할 숫자가 있는 동안
+        p = qu.pop(0)        # 큐에서 처리 대상을 하나 꺼내
+        print(p)             # 숫자를 출력
+        for x in g[p]:       # 관련 있는 숫자들 중에
+            if x not in done:   # 아직 큐에 추가된 적이 없는 숫자를
+                qu.append(x)    # 큐에 추가
+                done.add(x)     # 집합에도 추가
                 
 def print_all_number2(g, start):
     
